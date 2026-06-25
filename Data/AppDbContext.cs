9c2f7b47a6e8b2d1d088b60b809fbc2bb6432b2e
diegun99 <diegun123456789@gmail.com>
2026-06-24 21:24:40 -0500

    primer config

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : IdentityDbContext<ApplicationUser>
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+	{
+	}
+
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
+		base.onModelCreating(builder);
+	}
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+// Models/ApplicationUser.cs
+using Microsoft.AspNetCore.Identity;
+
+public class ApplicationUser : IdentityUser
+{
+	public ApplicationUser()
+	{
+
+	}
+
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? GoogleId { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+
+}
+
+
